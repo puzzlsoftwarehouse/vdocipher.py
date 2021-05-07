12ee4085ed72024b08168ed59b7cbe5f92663d22
Hellyson Ferreira <37805353+Hellyson-Ferreira@users.noreply.github.com>
2021-05-06 23:26:25 -0300

    V0.4.0:
        Updated:
    	- README.md
        Added:
    	- Annotations to the method opt

--- vdocipher/resources/otp.py
+++ vdocipher/resources/otp.py
@@ -1,8 +1,10 @@
 import json
 from dataclasses import dataclass, field
+from typing import List
 
 from dataclasses_json import dataclass_json, config
 
+from vdocipher.resources.annotate import Annotate
 from vdocipher.resources.request import post
 from vdocipher.resources.routes.base import VIDEOS
 
@@ -12,9 +14,12 @@ from vdocipher.resources.routes.base import VIDEOS
 class OTP:
     otp: str = None
     playback_info: str = field(metadata=config(field_name="playbackInfo"), default=None)
+    annotations: List[Annotate] = None
 
     def create(self, video_id: str, ttl: int = 300) -> 'OTP':
         data = {'ttl': ttl}
+        if self.annotations:
+            data['annotate'] = [annotate.to_dict() for annotate in self.annotations]
 
         response = post(url=f'{VIDEOS}/{video_id}/otp', data=json.dumps(data))
 
--- vdocipher/tests/test_otp.py
+++ vdocipher/tests/test_otp.py
@@ -1,10 +1,29 @@
+from vdocipher.resources.annotate import Annotate
 from vdocipher.resources.otp import OTP
 
 
 class TestOTP:
 
-    def test_video_get_list(self, vdocipher, video):
-
+    def test_create_otp(self, vdocipher, video):
         otp = vdocipher.OTP().create(video_id=video.id)
 
         assert isinstance(otp, OTP)
+
+    def test_create_otp_with_annotate(self, vdocipher, video):
+        annotate = Annotate(
+            annotation_type='rtext',
+            text='name',
+            alpha='0.60',
+            x='10',
+            y='10',
+            color='0xFF0000',
+            size='15',
+            interval='5000',
+            skip='200'
+        )
+        annotate_list = [annotate for _ in range(4)]
+        otp = vdocipher.OTP(annotations=annotate_list).create(video_id=video.id)
+
+        assert isinstance(otp, OTP)
+        assert otp.otp
+        assert otp.playback_info
