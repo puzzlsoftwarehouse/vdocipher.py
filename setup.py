2b5c813b25febe0f873bcab2546b8fa0e20446be
= <=>
2021-05-10 19:12:48 -0300

    V0.4.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='vdocipher.py',
-    version='0.4.6',
+    version='0.4.7',
     url='https://github.com/puzzlsoftwarehouse/vdocipher.py',
     license='MIT License',
     author='Puzzl Software House',
